e0dbac50bab2834e926b7cd2bf94b757a7db94e7
Pablo <pablogoncalves@unifucamp.edu.br>
2025-10-07 21:06:23 -0300

    Gerei os getters and setters necessarios.

--- todo-list/src/todoClass/Inicio.java
+++ todo-list/src/todoClass/Inicio.java
@@ -4,4 +4,24 @@ public class Inicio {
     private String nome;
     private String descricao;
     private Boolean concluida;
+    
+    
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Boolean getConcluida() {
+		return concluida;
+	}
+	public void setConcluida(Boolean concluida) {
+		this.concluida = concluida;
+	}
 }
